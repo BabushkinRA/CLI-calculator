2b8b8133b7c26d5594d01cd0ebcdbebcb6b985e3
Roman <fomeroma2003@gmail.com>
2025-07-24 22:45:49 +0300

    Добавляю то, что забыл сохратить

--- src/main/Java/InputReader.java
+++ src/main/Java/InputReader.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class InputReader {
-    public int[] readNumbers() {
+    public  int[] readNumbers() {
         int[] nums = new int[2];
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите первое число: ");
@@ -10,5 +10,19 @@ public class InputReader {
         System.out.println("Введите второе число: ");
         nums[1] = sc.nextInt();
         return nums; 
+    }
+ 
+    public String readOperation() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("""
+        Введите неодбходимую операцию:
+        + - сложение
+        - - вычетание
+        * - умножение
+        / - деление
+        
+        """);
+        String operation = sc.nextLine();
+        return operation;
     }
 }
